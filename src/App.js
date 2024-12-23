4a4df7a8ac1f7b45ad808baad996b764b27836e9
asimov <seyranmustafa4@gmail.com>
2024-12-24 01:26:18 +0300

    p

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Public from './components/Public'
-import Login from './features/auth/Login';
-import DashLayout from './components/DashLayout'
-import Welcome from './features/auth/Welcome'
-import NotesList from './features/notes/NotesList'
-import UsersList from './features/users/UsersList'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
 
 function App() {
   return (
@@ -15,7 +15,6 @@ function App() {
         <Route path="login" element={<Login />} />
 
         <Route path="dash" element={<DashLayout />}>
-
           <Route index element={<Welcome />} />
 
           <Route path="notes">
@@ -25,9 +24,8 @@ function App() {
           <Route path="users">
             <Route index element={<UsersList />} />
           </Route>
-
-        </Route>{/* End Dash */}
-
+        </Route>
+        {/* End Dash */}
       </Route>
     </Routes>
   );
