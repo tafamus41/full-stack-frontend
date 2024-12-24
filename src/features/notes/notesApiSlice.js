83a442dee8e3afd79a13603b3e5dd9f9dd9461fa
asimov <seyranmustafa4@gmail.com>
2024-12-25 00:47:22 +0300

    p

--- src/features/notes/notesApiSlice.js
+++ src/features/notes/notesApiSlice.js
@@ -4,6 +4,7 @@ import {
 } from "@reduxjs/toolkit";
 import { apiSlice } from "../../app/api/apiSlice"
 
+
 const notesAdapter = createEntityAdapter({
     sortComparer: (a, b) => (a.completed === b.completed) ? 0 : a.completed ? 1 : -1
 })
