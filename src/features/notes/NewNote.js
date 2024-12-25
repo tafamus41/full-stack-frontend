0d829dc02b618604ee510c785ac25ea35202a7a2
asimov <seyranmustafa4@gmail.com>
2024-12-26 00:21:50 +0300

    p

--- src/features/notes/NewNote.js
+++ src/features/notes/NewNote.js
@@ -4,7 +4,6 @@ import NewNoteForm from './NewNoteForm'
 
 const NewNote = () => {
     const users = useSelector(selectAllUsers)
-
     const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
 
     return content
